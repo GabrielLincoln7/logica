6c4dfd082900f41b6a98f41d0485b4feb5075d4c
GabrielLincoln7 <50593828+GabrielLincoln7@users.noreply.github.com>
2019-05-13 15:28:41 -0300

    dados: primeira versão
    oi

---
+++ dados.py
@@ -0,0 +1,13 @@
+from random import randint
+
+print("Jogo de Dados")
+
+dados1 = randint(1, 6)
+dados2 = randint(1, 6)
+dados3 = randint(1, 6)
+dados4 = randint(1, 6)
+
+print("dados 1:", dados1)
+print("dados 2:", dados2)
+print("dados 3:", dados3)
+print("dados 4:", dados4)   
